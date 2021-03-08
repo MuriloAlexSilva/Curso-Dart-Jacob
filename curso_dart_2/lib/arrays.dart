9cb070a02498d745f89c7be2ed917ca403bd3445
Murilo Silva <murilo.alex.silva@gmail.com>
2021-03-08 16:49:46 -0300

    Inserindo arrays, while e for

---
+++ curso_dart_2/lib/arrays.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+main() {
+  bool condicao = true;
+  var nome = [];
+
+  print('============Programa Iniciado============');
+  print('\n'); //Para pular linha
+  while (condicao) {
+    print('Digite os nomes desejados');
+    String text = stdin.readLineSync();
+    if (text == 'sair') {
+      print('Sua lista de nomes $nome');
+      print(
+          'O tamanho da sua lista de nomes: ${nome.length} nomes adicionados');
+      print('\n'); //Para pular linha
+      print('============Programa Finalizado============');
+      print('\n'); //Para pular linha
+
+      condicao = false;
+    } else if (text == 'remove') {
+      print('Digite o nome que deseja remover: ');
+      print('Sua lista de nomes $nome');
+      String nomeRemove = stdin.readLineSync();
+      nome.remove(nomeRemove);
+      print('Nome removido');
+    } else if (text == 'print') {
+      print('Sua lista de nomes $nome');
+    } else {
+      nome.add(text);
+    }
+  }
+}
--- curso_dart_2/lib/while.dart
+++ curso_dart_2/lib/while.dart
@@ -3,8 +3,9 @@ main() {
   int x = 0;
   //A diferença do while, seria que enquanto o valor corresponder a variavel ele vai rodar
   while (condicao) {
+    print("Estou printando o $x");
     if (x > 9) {
-      print("Estou printando o $x");
+      condicao = false;
     }
     x++;
   }
